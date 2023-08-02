9ce3185583d17eda416e845019ab7a3048c0498a
Rémi Chainey <remichainey@gmail.com>
2023-08-02 15:24:13 +0200

    Ajout manuelle des combattants

--- tkinter, pokédex.py
+++ tkinter, pokédex.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageTk
 from tkinter import messagebox
 
 fenetre = tk.Tk()
-fenetre.geometry("1024x768")
+fenetre.geometry("1000x800")
 fenetre.title("Pokédex")
 
 class DB:
@@ -11,9 +11,7 @@ class DB:
             self.nom = nom
             self.type = type
             self.capacités = capacités
-            # self.image = ImageTk.PhotoImage(Image.open("images/" + self.nom + ".png"))
-
-
+            
 
 sangoku = DB("san goku", "saiyan", "téléportation")
 sangohan = DB("san gohan", "saiyan", "malin comme un singe")
@@ -21,14 +19,24 @@ krilin = DB("krilin", "humain", "a un cyborg comme femme")
 
 persos = [sangoku, sangohan, krilin]
 
-
 def affiche_detail():
     indexDuPerso = int(listbox.curselection()[0])
     nom_label.config(text=f"nom {persos[indexDuPerso].nom}")
-    #  type_label = tk.Label(fenetre, text="type " + persos[indexDuPerso].type)
-    #  capacités_label = tk.Label(fenetre, text="capacités " + persos[indexDuPerso].capacités)
-      
-      
+    type_label.config(text=f"type {persos[indexDuPerso].type}")
+    capacités_label.config(text=f"capacités {persos[indexDuPerso].capacités}")
+    
+   
+def ajouter_combattant():
+     nom = nom_input_label.get()
+     type = type_input_label.get()
+     capacités = capacités_input_label.get()
+     persos.append(DB(nom, type, capacités))
+     listbox.insert(tk.END, nom)
+     nom_input_label.delete(0, tk.END)
+     type_input_label.delete(0, tk.END)
+     capacités_input_label.delete(0, tk.END)
+
+     messagebox.showinfo("Ajout réussi", "Combattant ajouté")
 
  #création d'un logo sur fenetre tkinter
 image = Image.open("Dragon-Ball-Logo.png")
@@ -37,45 +45,54 @@ image = ImageTk.PhotoImage(image)
 label = tk.Label(fenetre, image=image)
 label.place(x=10, y=10)
 
-nom_label = tk.Label(fenetre, text="dd ")
-type_label = tk.Label(fenetre, text=" d")
-capacités_label = tk.Label(fenetre, text=" d")
-nom_label.place(x=20, y=30)
-# type_label.pack()
-# capacités_label.pack()
+nom_label = tk.Label(fenetre, text=" ")
+type_label = tk.Label(fenetre, text=" ")
+capacités_label = tk.Label(fenetre, text=" ")
+nom_label.place(x=300, y=500)
+type_label.place(x=300, y=520)
+capacités_label.place(x=300, y=540)
 
  #creation d'une liste déroulante
 listbox = tk.Listbox(fenetre)
-listbox.place(x=800, y=300)
+listbox.place(x=800, y=200)
 
-#ajout de pokemon à la liste déroulante
+ #ajout de pokemon à la liste déroulante
 listbox.insert(tk.END, "sangoku")
 listbox.insert(tk.END, "sangohan")
 listbox.insert(tk.END, "krilin")
 
 
 
-#création d'un bouton por récupurer la valeur saisie
+ #création d'un bouton por récupurer la valeur saisie
 button = tk.Button(fenetre, text="Afficher les détails", command=affiche_detail)
-button.place(x=810, y=464)
+button.place(x=810, y=367)
 
 
-DB_frame = tk.Frame(fenetre)
-DB_frame.place()
-
-nom_label = tk.Label(DB_frame, text="")
-nom_label.pack()
-
-type_label = tk.Label(DB_frame, text="")
-type_label.pack
-
-capacités_label = tk.Label(DB_frame, text="")
-capacités_label.pack()
+ #création d'un bouton "ajout de personnage"
+button = tk.Button(fenetre, text="Ajouter un personnage", command=ajouter_combattant)
+button.place(x=810, y=700)
 
 
-# création d'un champ de saisie
-champ_saisie = tk.Entry(fenetre)
-champ_saisie.place(x=800, y=491)
+ #creation des champs de saisie "Nouveau combattant"
+nouveau_combattant = tk.Frame(fenetre)
+nouveau_combattant.place(x=780, y=500)
+
+nom_input_label = tk.Label(nouveau_combattant, text="Nom du combattant")
+nom_input_label.grid(row=0, column=0, padx=10, pady=1)
+nom_input_label = tk.Entry(nouveau_combattant)
+nom_input_label.grid(row=1, column=0, padx=10, pady=1)
+
+type_input_label = tk.Label(nouveau_combattant, text="Type du combattant")
+type_input_label.grid(row=2, column=0, padx=10, pady=1)
+type_input_label = tk.Entry(nouveau_combattant)
+type_input_label.grid(row=3, column=0, padx=10, pady=1)
+
+capacités_input_label = tk.Label(nouveau_combattant, text="Capacités du combattant")
+capacités_input_label.grid(row=4, column=0, padx=10, pady=1)
+capacités_input_label = tk.Entry(nouveau_combattant)
+capacités_input_label.grid(row=5, column=0, padx=10, pady=1)
+
+
 
 
 
